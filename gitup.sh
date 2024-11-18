898ba9203e1568ae98891d327675593c220ac286
lxwooxy <georginawooxy@gmail.com>
2024-11-17 20:14:12 -0500

    prism time

---
+++ gitup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add all changes to the staging area
+git add .
+
+# Commit with the message "prism time"
+git commit -m "prism time"
+
+# Push the changes to the remote repository
+git push
